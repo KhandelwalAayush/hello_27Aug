bc3c293b628e8a49922ae02986e1c5c81aa00e53
hp <hp@LAPTOP-95IQJISM>
2020-08-27 18:28:30 +0530

    commit_18:28PM

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World!!");
-		System.out.println("Hello Chandigarh!!");
+		System.out.println("Hello!!");
 	}
 
 }
